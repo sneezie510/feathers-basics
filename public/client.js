5e543ae0dbb82f0867e793f6c45955dc8bec1645
sneezie510 <shuynh1300@yahoo.com>
2018-04-25 16:10:51 +0200

    The basics for FeathersJS, creating messages, pagination and a counter list. Set up feathers-memory for an in-memory database.

---
+++ public/client.js
@@ -0,0 +1,79 @@
+const app = feathers();
+const memory = require('feathers-memory');
+
+// Register a simple todo service that return the name and a text
+app.use('todos', {
+  async get(name) {
+    // Return an object in the form of { name, text }
+    return {
+      name,
+      text: `You have to do ${name}`
+    };
+  }
+});
+
+// A function that gets and logs a todo from the service
+async function logTodo(name) {
+  // Get the service we registered above
+  const service = app.service('todos');
+  // Call the `get` method with a name
+  const todo = await service.get(name);
+
+  // Log the todo we got back
+  console.log(todo);
+}
+
+logTodo('dishes');
+
+app.use('messages', feathers.memory({
+  paginate: {
+    default: 10,
+    max: 25
+  }
+}));
+
+async function createAndFind() {
+  // Stores a reference to the messages service so we don't have to call it all the time
+  const messages = app.service('messages');
+
+  for (let counter = 0; counter < 100; counter++) {
+    await messages.create({
+      counter,
+      message: `Message number ${counter}`
+    });
+  }
+
+  // We show 10 entries by default. By skipping 10 we go to page 2
+  const page2 = await messages.find({
+    query: { $skip: 10 }
+  });
+
+  console.log('Page number 2', page2);
+
+  // Show 20 items per page
+  const largePage = await messages.find({
+    query: { $limit: 20 }
+  });
+
+  console.log('20 items', largePage);
+
+  // Find the first 10 items with counter greater 50 and less than 70
+  const counterList = await messages.find({
+    query: {
+      counter: { $gt: 50, $lt: 70 }
+    }
+  });
+
+  console.log('Counter greater 50 and less than 70', counterList);
+
+  // Find all entries with text "Message number 20"
+  const message20 = await messages.find({
+    query: {
+      message: 'Message number 20'
+    }
+  });
+
+  console.log('Entries with text "Message number 20"', message20);
+}
+
+createAndFind();
