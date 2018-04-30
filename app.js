e50dfe6d1e4f75eab7406e5c09dba904b2879888
sneezie510 <shuynh1300@yahoo.com>
2018-04-30 15:04:49 +0200

    Remove the now ignored directory node_modules. Clients section on Feathers.js basics - transparently connected to another Feathers server and use its services.

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const express = require('@feathersjs/express');
 const socketio = require('@feathersjs/socketio');
 const memory = require('feathers-memory');
 
+// This creates an app that's both an Express and Feathers app
 const app = express(feathers());
+
+// Enable CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 // Turn on JSON body parsing for REST services
 app.use(express.json())
--- public/client.js
+++ public/client.js
@@ -1,19 +1,25 @@
+// Create a Feathers application
 const app = feathers();
 
-// Create a websocket connecting to our Feathers server
-const socket = io('http://localhost:3030');
+// Initialize a REST connection
+const rest = feathers.rest('http://localhost:3030');
+
+// Configure the REST client to use 'window.fetch'
+app.configure(rest.fetch(window.fetch));
 
 // Listen to new messages being created
-socket.on('messages created', message =>
-  console.log('Someone created a message', message)
-);
-
-socket.emit('create', 'messages', {
-  text: 'Hello from socket'
-}, (error, result) => {
-  if (error) throw error
-  socket.emit('find', 'messages', (error, messageList) => {
-    if (error) throw error
-    console.log('Current messages', messageList);
-  });
+app.service('messages').on('created', message => {
+  console.log('Created a new message locally', message);
 });
+
+async function createAndList() {
+  await app.service('messages').create({
+    text: 'Hello from Feathers browser client'
+  });
+
+  const messages = await app.service('messages').find();
+
+  console.log('Messages', messages);
+}
+
+createAndList();
